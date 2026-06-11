eda976098af76e8c3404ab36d0f5c287a577f505
TommyDuong248 <37056483+TommyDuong248@users.noreply.github.com>
2026-06-11 09:53:42 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using GioChuanGiangDay.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+await SeedHoatDongQuyDoi.RunAsync(app.Services);
+
+app.Run();
